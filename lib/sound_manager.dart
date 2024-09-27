4e5e6ab1e4beb228a45d3085c4bbf3fdd9ecd4a2
Farwah Shamshir <farwahshamshir@live.com>
2024-09-27 20:52:52 +0500

    audio plugin change

--- lib/sound_manager.dart
+++ lib/sound_manager.dart
@@ -1,8 +1,14 @@
-import 'package:audioplayers/audioplayers.dart';
+//import 'package:audioplayers/audioplayers.dart';
+import 'package:just_audio/just_audio.dart';
 
 class SoundManager {
   final AudioPlayer _player = AudioPlayer();
-  void playSound(int soundNumber) {
-    _player.play(AssetSource('assets/not$soundNumber.mp3'));
+
+  void playSound(int soundNumber) async {
+    // Load the asset sound
+    await _player.setAsset('assets/not$soundNumber.mp3');
+    // Play the loaded sound
+    _player.play();
   }
 }
+
